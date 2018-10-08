4cc5ed90bb6d3c8b61ced286660eb31797088f08
yunting0707 <yunting0707@users.noreply.github.com>
2018-10-07 22:52:01 -0400

    draft
    cannot make the castle a prefab

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour {
+
+    static public bool goalMet = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        // When the trigger is hit by something
+        // Check to see if it's a Projectile
+        if (other.gameObject.tag == "Projectile")
+        {
+            // If so, set goalMet to true
+            Goal.goalMet = true;
+            // Also set the alpha of the color to higher opacity
+            Material mat = GetComponent<Renderer>().material;
+            Color c = mat.color;
+            c.a = 1;
+            mat.color = c;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
